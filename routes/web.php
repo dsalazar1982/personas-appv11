b71960e17824ddc0e84756042c1c1ff7797c7cbb
Daniel Alberto Salazar Erazo <dsalazar1982@yahoo.com>
2025-04-22 15:16:27 -0500

    Creacion ComunaController, HomeController, Modelo Comuna, Vista Index Comuna
    Se crea el controlador para Comuna y Home asi como el modelo de Comuna y
    la vista Index paa Comuna, se modifica las rutas en web.php y se agrega
    la ruta /comunas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ComunaController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+
+Route::get('/comunas', [ComunaController::class, 'index']);
